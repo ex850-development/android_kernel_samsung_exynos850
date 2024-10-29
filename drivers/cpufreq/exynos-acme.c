fcb008be1eeabba6ce9e8208ad3bb09f2ec5c7d5
danya2271 <danya2271@yandex.ru>
2024-10-30 02:12:23 +0500

    drivers: cpufreq: exynos-acme: Undervolt by -100mV

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -1137,6 +1137,9 @@ static __init int init_table(struct exynos_cpufreq_domain *domain)
 	for (index = 0; index < domain->table_size; index++) {
 		domain->freq_table[index].driver_data = index;
 
+		/* Undervolt with uV value */
+		volt_table[index] -= 100000;
+
 		if (table[index] > domain->max_freq)
 			domain->freq_table[index].frequency = CPUFREQ_ENTRY_INVALID;
 		else if (table[index] < domain->min_freq)
